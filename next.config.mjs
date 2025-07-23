51c1b8404878ac9810558436ece012d43fdc38c3
Sistemas <ldj821@gmail.com>
2025-07-23 10:32:58 -0600

    nextauth2

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,26 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        remotePatterns: [
-          {
-            protocol: 'https',
-            hostname: 'placehold.co',
-            port: ""
-          },
-        ],
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+        port: ""
       },
+    ],
+  },
+  // Añade estas configuraciones para Prisma:
+  webpack: (config) => {
+    // Excluir Prisma del bundle del cliente
+    config.externals = [...(config.externals || []), { 
+      '@prisma/client': '@prisma/client',
+      '.prisma/client': '.prisma/client'
+    }];
+    return config;
+  },
+  experimental: {
+    serverComponentsExternalPackages: ["@prisma/client"],
+  }
 };
 
 export default nextConfig;
